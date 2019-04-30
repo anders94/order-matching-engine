77dd1385e05f78448133b82aff571c2735534209
Anders Brownworth <anders@Anderss-MacBook-Pro.local>
2019-04-30 07:05:10 -0400

    test.js add pg

--- test.js
+++ test.js
@@ -1,7 +1,29 @@
+const fs = require('fs');
 const test = require('tape');
 const async = require('async');
+const { Client } = require('pg');
+
+let config;
+let client;
 
 async.series([
+    function(cb) {
+	let data = fs.readFileSync('database.json');
+	let e;
+	try {
+	    config = JSON.parse(data);
+	}
+	catch(_e) {
+	    e = _e;
+	}
+	finally {
+	    cb(e);
+	}
+    },
+    function(cb) {
+	client = new Client({database: config.test.database, user: config.test.user, password: config.test.password});
+	client.connect(cb);
+    },
     function(cb) {	
 	console.log('*****************************');
 	console.log('**** unit tests');
@@ -10,8 +32,11 @@ async.series([
 	cb();
     },
     function(cb) {
+	client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+		console.log(err ? err.stack : res.rows[0].message);
+		cb(err);
+	    });
 	// unit tests here
-	cb();
     },
     function(cb) {
 	console.log('*****************************');
@@ -26,5 +51,6 @@ async.series([
     }],
     function(err) {
 	if (err) console.error(err);
+	client.end();
 	console.log('done');
     });
