b364f12556f9440eafb17065b52d2e61b3c2b67a
Anders Brownworth <anders@Anderss-MacBook-Pro.local>
2019-04-30 21:25:38 -0400

    make test data destruction agnostic to order price and size because some tests change those

--- migrations/test-data/sqls/20190429141424-initial-test-data-down.sql
+++ migrations/test-data/sqls/20190429141424-initial-test-data-down.sql
@@ -1,11 +1,7 @@
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-a@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='buy' AND price = 4990.0 AND volume = 1.213;
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-a@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='buy' AND price = 4995.0 AND volume = 0.902;
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-b@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='buy' AND price = 4995.0 AND volume = 0.283;
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-b@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='buy' AND price = 4999.5 AND volume = 1.121;
-
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-c@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='sell' AND price = 5001.0 AND volume = 0.816;
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-d@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='sell' AND price = 5005.0 AND volume = 1.375;
-DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-d@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD') AND side ='sell' AND price = 5010.0 AND volume = 0.923;
+DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-a@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD');
+DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-b@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD');
+DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-c@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD');
+DELETE FROM offers WHERE user_id = (SELECT id FROM users WHERE email='user-d@example.com') AND market_id = (SELECT id FROM markets WHERE base_symbol='BTC' and quote_symbol='USD');
 
 DELETE FROM markets WHERE base_symbol = 'BTC' AND quote_symbol = 'USD';
 DELETE FROM markets WHERE base_symbol = 'ETH' AND quote_symbol = 'USD';
