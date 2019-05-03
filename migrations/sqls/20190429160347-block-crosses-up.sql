13adc4cce3c72c8a508782a58410b656350ebb80
Anders Brownworth <anders-git@evantide.com>
2019-05-03 16:36:40 +0000

    Make crossed book check ignore filled offers.

--- migrations/sqls/20190429160347-block-crosses-up.sql
+++ migrations/sqls/20190429160347-block-crosses-up.sql
@@ -5,11 +5,11 @@
 CREATE OR REPLACE FUNCTION block_crosses()
 RETURNS trigger AS $function$
 BEGIN
-  IF NEW.side = 'buy' AND NEW.price >= (SELECT price FROM offers WHERE market_id = NEW.market_id AND side = 'sell' ORDER BY price ASC LIMIT 1) THEN
-    RAISE NOTICE 'This order would result in a crossed book.';
+  IF NEW.side = 'buy' AND NEW.price >= (SELECT price FROM offers WHERE market_id = NEW.market_id AND side = 'sell' AND unfilled > 0.0 AND active = TRUE ORDER BY price ASC LIMIT 1) THEN
+    RAISE EXCEPTION 'This order would result in a crossed book.';
     RETURN NULL;
-  ELSIF NEW.side = 'sell' AND NEW.price <= (SELECT price FROM offers WHERE market_id = NEW.market_id AND side = 'buy' ORDER BY price DESC LIMIT 1) THEN
-    RAISE NOTICE 'This order would result in a crossed book.';
+  ELSIF NEW.side = 'sell' AND NEW.price <= (SELECT price FROM offers WHERE market_id = NEW.market_id AND side = 'buy' AND unfilled > 0.0 AND active = TRUE ORDER BY price DESC LIMIT 1) THEN
+    RAISE EXCEPTION 'This order would result in a crossed book.';
     RETURN NULL;
   END IF;
   RETURN NEW;
